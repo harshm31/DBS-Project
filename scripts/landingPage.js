51c47ef7f9c2011dcb0f2ea902aa8b4a51143722
harshm31 <harshmutha31@gmail.com>
2018-10-06 17:17:20 +0530

    first commit

---
+++ scripts/landingPage.js
@@ -0,0 +1,40 @@
+function redirectToHome()
+{
+	$(".landingPage_button").click(function()
+	{
+		console.log("clicked");
+		var url = "http://www.goodreads.com";
+    	window.location.href = url;
+	});
+}
+
+function animateText()
+{
+
+ 	$('.ml2').each(function(){
+  		$(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+	});
+
+	anime.timeline({loop: true})
+  		.add({
+	    targets: '.ml2 .letter',
+	    opacity: [0,1],
+	    easing: "easeInOutQuad",
+	    duration: 2250,
+	    delay: function(el, i) {
+	      return 150 * (i+1)
+	    }
+  	}).add({
+	    targets: '.ml2',
+	    opacity: 0,
+	    duration: 1000,
+	    easing: "easeOutExpo",
+	    delay: 1000
+  	});
+}
+
+$(document).ready(function()
+{
+	redirectToHome();
+	animateText();
+});
